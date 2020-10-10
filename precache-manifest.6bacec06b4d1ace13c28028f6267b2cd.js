12e542b8e3924a19ebcb451246a11294aa46abba
Leonardo Cunha <leonardo.cunha@objectedge.com>
2020-10-10 04:12:48 -0300

    Updates

---
+++ precache-manifest.6bacec06b4d1ace13c28028f6267b2cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29a110aeb9e10ca8f94c4581d3eb1b8b",
+    "url": "/sync/index.html"
+  },
+  {
+    "revision": "eb77a98bf6630c8b3762",
+    "url": "/sync/static/css/main.7b3866d9.chunk.css"
+  },
+  {
+    "revision": "be045f09f2757ce19409",
+    "url": "/sync/static/js/2.acc35a9d.chunk.js"
+  },
+  {
+    "revision": "0445d5183d0141ac77c6557b72598011",
+    "url": "/sync/static/js/2.acc35a9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb77a98bf6630c8b3762",
+    "url": "/sync/static/js/main.bcb019bb.chunk.js"
+  },
+  {
+    "revision": "dbcd81526a35e6f635ca",
+    "url": "/sync/static/js/runtime-main.cbc6bef5.js"
+  }
+]);
