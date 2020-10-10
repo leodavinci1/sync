b78209a516694c72c4ad51e86df39dab151a1b5b
Leonardo Cunha <leonardo.cunha@objectedge.com>
2020-10-10 04:18:24 -0300

    Add arquivos

---
+++ build/precache-manifest.f98fedd715809598543788642aa798b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c4cb71df1e4efbf50d5c3f6b0b07687",
+    "url": "/sync/index.html"
+  },
+  {
+    "revision": "15739a1bc4b4c12bd39c",
+    "url": "/sync/static/css/main.7b3866d9.chunk.css"
+  },
+  {
+    "revision": "be045f09f2757ce19409",
+    "url": "/sync/static/js/2.acc35a9d.chunk.js"
+  },
+  {
+    "revision": "0445d5183d0141ac77c6557b72598011",
+    "url": "/sync/static/js/2.acc35a9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15739a1bc4b4c12bd39c",
+    "url": "/sync/static/js/main.ffcfb2a7.chunk.js"
+  },
+  {
+    "revision": "dbcd81526a35e6f635ca",
+    "url": "/sync/static/js/runtime-main.cbc6bef5.js"
+  }
+]);
